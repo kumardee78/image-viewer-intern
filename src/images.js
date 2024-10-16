eccff2fa4a9a44d59293396ff3c1b24baf1fab5d
kumardee78 <deedancer951@gmail.com>
2024-10-16 14:05:41 +0530

    image chage

--- src/images.js
+++ src/images.js
@@ -1,13 +1,13 @@
 const images = [
-    'src/assets/1.jpg',
-    'src/assets/2.jpg',
-    'src/assets/3.jpg',
-    'src/assets/4.jpg',
-    'src/assets/5.jpg',
-    'src/assets/6.jpg',
-    'src/assets/7.jpg',
-    'src/assets/8.jpg',
-    'src/assets/9.jpg',
-    'src/assets/10.jpg'
+    'public/1.jpg',
+    'public/2.jpg',
+    'public/3.jpg',
+    'public/4.jpg',
+    'public/5.jpg',
+    'public/6.jpg',
+    'public/7.jpg',
+    'public/8.jpg',
+    'public/9.jpg',
+    'public/10.jpg'
 ]
 export default images;
