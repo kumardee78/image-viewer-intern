010b860cdf53169fcb9ad756ca12a2959660426b
kumardee78 <deedancer951@gmail.com>
2024-10-16 14:09:16 +0530

    ./

--- src/images.js
+++ src/images.js
@@ -1,13 +1,13 @@
 const images = [
-    'public/1.jpg',
-    'public/2.jpg',
-    'public/3.jpg',
-    'public/4.jpg',
-    'public/5.jpg',
-    'public/6.jpg',
-    'public/7.jpg',
-    'public/8.jpg',
-    'public/9.jpg',
-    'public/10.jpg'
+    '/1.jpg',
+    '/2.jpg',
+    '/3.jpg',
+    '/4.jpg',
+    '/5.jpg',
+    '/6.jpg',
+    '/7.jpg',
+    '/8.jpg',
+    '/9.jpg',
+    '/10.jpg'
 ]
 export default images;
